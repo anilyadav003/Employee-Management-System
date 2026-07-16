095ba0d5e880852cc06877c35da1e11ff3bf2c3e
aniyadav003 <anilyadav110110@gmail.com>
2026-07-16 21:53:22 +0530

    Complete Role CRUD module with getRoleByName API

--- backend/src/main/java/com/anilyadav/ems/controller/RoleController.java
+++ backend/src/main/java/com/anilyadav/ems/controller/RoleController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.anilyadav.ems.enums.RoleType;
 
 import java.util.List;
 
@@ -54,6 +55,14 @@ public class RoleController {
         roleService.deleteRole(id);
 
         return ResponseEntity.ok("Role deleted successfully.");
+    }
+    @GetMapping("/name/{roleType}")
+    public ResponseEntity<RoleResponse> getRoleByName(
+            @PathVariable RoleType roleType) {
+
+        RoleResponse roleResponse = roleService.getRoleByName(roleType);
+
+        return ResponseEntity.ok(roleResponse);
     }
 
 }
