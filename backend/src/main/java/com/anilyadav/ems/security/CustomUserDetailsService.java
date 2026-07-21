5f87c072f380874780f9c6bc72be00a0f42a684b
aniyadav003 <anilyadav110110@gmail.com>
2026-07-21 19:42:21 +0530

    Implement JWT service and Spring Security foundation

---
+++ backend/src/main/java/com/anilyadav/ems/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.anilyadav.ems.security;
+
+import com.anilyadav.ems.entity.auth.User;
+import com.anilyadav.ems.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found with username: " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
